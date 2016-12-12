e020827bedcf15795b294e73117642e47baeeb8b
ytelloUTC <elizatello39@gmail.com>
2016-12-12 09:04:28 -0500

    ytelloUtc3

--- Producto2ytello/app/models/Cliente.java
+++ Producto2ytello/app/models/Cliente.java
@@ -1,5 +1,35 @@
 package models;
 
-public class Cliente {
+import java.util.List;
 
+import javax.persistence.*;
+
+import com.avaje.ebean.Model;
+
+import play.data.format.*;
+import play.data.validation.*;
+
+import com.avaje.ebean.*;
+@Entity 
+public class Cliente extends Model {
+	 private static final long serialVersionUID = 1L;
+
+		@Id
+	    public Long id;
+	    
+	    @Constraints.Required
+	    public String nombre;
+	    @Constraints.Required
+	    public String apellido;
+	    @Constraints.Required
+	    public String direccion;
+	    @Constraints.Required
+	    public String telefono;
+	    
+	    
+	   public static Find<Long,Cliente> find = new Find<Long,Cliente>(){};
+		public static List<Cliente> listadoCliente() {
+			// TODO Auto-generated method stub
+			return find.all();
+		}
 }
